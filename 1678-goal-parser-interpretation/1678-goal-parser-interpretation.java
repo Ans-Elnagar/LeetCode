9e23ea30bed5750b14970597275fee9f5c06a850
Ans Elnagar <ansgom3a@gmail.com>
2022-09-25 17:34:45 +0200

    Time: 0 ms (100.00%), Space: 39.9 MB (99.65%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,20 +1,15 @@
 class Solution {
     public String interpret(String command) {
-        Map<String, String> map = new HashMap<>();
-        map.put("G", "G");
-        map.put("()", "o");
-        map.put("(al)", "al");
         int len = command.length();
         StringBuilder result = new StringBuilder();
         for(int i=0; i<len; i++){
             if(command.charAt(i) == '('){
-                String segment = "";
-                for(; i<len; i++){
-                    segment += command.charAt(i);
-                    if(command.charAt(i) == ')'){
-                        result.append(map.get(segment));
-                        break;
-                    }
+                if(command.charAt(i+1) == ')' ){
+                    i++;
+                    result.append("o");
+                }else{
+                    i += 3;
+                    result.append("al");
                 }
             }else{
                 result.append("G");
