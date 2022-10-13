b3da7329dc0c389a47e8190189eaa1f612fa0b5d
Ans Elnagar <ansgom3a@gmail.com>
2022-10-13 15:13:59 +0200

    Time: 5 ms (33.68%), Space: 54 MB (67.68%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] findBall(int[][] grid) {
+        int height = grid.length;
+        int width = grid[0].length;
+        if(width == 1){
+            return new int[]{-1};
+        }
+        int[][] mem = new int[height+1][width];
+        for(int j=0; j<width; j++)
+            mem[height][j] = j;
+        for(int i=height-1; i>=0; i--){
+            if(grid[i][0] == -1 || grid[i][1] == -1)
+                mem[i][0] = -1;
+            else
+                mem[i][0] = mem[i+1][1];
+            
+            if(grid[i][width-1] == 1 || grid[i][width-2] == 1)
+                mem[i][width-1] = -1;
+            else
+                mem[i][width-1] = mem[i+1][width-2];
+            
+            for(int j=1; j<width-1; j++){
+                if(grid[i][j] == -1){
+                    if(grid[i][j-1] == 1)
+                        mem[i][j] = -1;
+                    else
+                        mem[i][j] = mem[i+1][j-1];
+                }else{
+                    if(grid[i][j+1] == -1)
+                        mem[i][j] = -1;
+                    else
+                        mem[i][j] = mem[i+1][j+1];
+                }
+            }
+        }
+        return mem[0];
+    }
+}
