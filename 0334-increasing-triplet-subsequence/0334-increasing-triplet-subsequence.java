f3eb1fe00dc2dc0b362bc6cea8b73df251cf3cdb
Ans Elnagar <ansgom3a@gmail.com>
2022-10-18 15:10:27 +0200

    Time: 11 ms (17.25%), Space: 147.7 MB (5.16%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -1,17 +1,16 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        int[] next = new int[nums.length];
-        TreeMap<Long, Integer> tree = new TreeMap<>();
-        tree.put(Long.MAX_VALUE, nums.length);
-        for(int i=nums.length-1; i>=0; i--){
-            long key = tree.ceilingKey((long)nums[i]+1);
-            next[i] = tree.get(key);
-            tree.put((long)nums[i], i);
-        }
-        for(int i=0; i<nums.length; i++){
-            if(next[i] != nums.length && next[next[i]] != nums.length)
-                return true;
-        }
-        return false;
+       int min = Integer.MAX_VALUE;
+       int secondMin = Integer.MAX_VALUE;
+       for(int num : nums) {
+           if(num <= min)
+               min = num;
+           else if(num <= secondMin) 
+               secondMin = num;
+           else 
+               // if greater than secondMin then also greater than the min previous to secondMin
+               return true;
+       }
+       return false;
     }
 }
