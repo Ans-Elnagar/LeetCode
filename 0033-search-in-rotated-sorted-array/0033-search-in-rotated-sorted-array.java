76f41e9e9737710e9430ea7b750d682d8febc871
Ans Elnagar <ansgom3a@gmail.com>
2022-10-20 13:46:45 +0200

    Time: 1 ms (80.82%), Space: 42.8 MB (35.26%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        if(nums.length == 1){
+            if(nums[0] == target)
+                return 0;
+            return -1;
+        }
+        int r = findRealStart(nums);
+        return Math.max(bs(nums, 0, r-1, target),
+                       bs(nums, r, nums.length-1, target));
+    }
+    private int findRealStart(int[] nums){
+        int low=1, high = nums.length-1, mid = 0;
+        int last = nums[high];
+        while(low<=high){
+            mid = (low+high)/2;
+            if(nums[mid] > last){
+                low = mid+1;
+                continue;
+            }
+            if(nums[mid] < nums[mid-1])
+                break;
+            high = mid - 1;
+        }
+        return mid;
+    }
+    private int bs(int[] nums, int low, int high, int target){
+        if(low > high)
+            return -1;
+        int mid = (low + high) / 2;
+        if(nums[mid] == target)
+            return mid;
+        if(nums[mid] < target)
+            return bs(nums, mid+1, high, target);
+        return bs(nums, low, mid-1, target);
+    }
+}
