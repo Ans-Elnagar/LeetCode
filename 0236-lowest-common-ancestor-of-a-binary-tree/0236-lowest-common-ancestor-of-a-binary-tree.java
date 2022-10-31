c658dd10dd7685e4e4d3e79f296b431afe709e2c
Ans Elnagar <ansgom3a@gmail.com>
2022-10-31 19:19:41 +0200

    Time: 24 ms (14.76%), Space: 48.1 MB (13.62%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        Queue<TreeNode> containsP = new LinkedList<>();
+        Queue<TreeNode> containsQ = new LinkedList<>();
+        doesContain(containsP, root, p);
+        doesContain(containsQ, root, q);
+        // making p always bigger or equal
+        if(containsP.size() < containsQ.size()){
+            Queue<TreeNode> temp = containsP;
+            containsP = containsQ;
+            containsQ = temp;
+        }
+        int diffLen = containsP.size() - containsQ.size();
+        while(diffLen-->0)
+            containsP.remove();
+        while(!containsP.isEmpty()){
+            TreeNode t1 = containsP.remove();
+            TreeNode t2 = containsQ.remove();
+            if(t1 == t2)
+                return t1;
+        }
+        return null;
+    }
+    private boolean doesContain(Queue<TreeNode> list, TreeNode node, TreeNode find){
+        if(node == null)
+            return false;
+        if(node.val == find.val){
+            list.add(node);
+            return true;
+        }
+        if(doesContain(list, node.left, find) || doesContain(list, node.right, find)){
+            list.add(node);
+            return true;
+        }
+        return false;
+    }
+}
